5fc5a1630ce7098ba8f8abeca1ec80ce68aeb5be
Hao-Kai Tseng <threebriers@gmail.com>
2024-01-26 19:17:58 +0000

    finalize heatmap & turkey

--- R_scripts/04_01_turkey_flu_and_strain.R
+++ R_scripts/04_01_turkey_flu_and_strain.R
@@ -9,13 +9,13 @@ library(readxl)
 library(scales)
 library(gridExtra)
 
-setwd("C:/Users/three/Neu Kasten_5440/022 master degree LSHTM/LSHTM/013 Data Challenge/My Sanofi_5440/data_challenge_2024_sanofi")
+#setwd("C:/Users/three/Neu Kasten_5440/022 master degree LSHTM/LSHTM/013 Data Challenge/My Sanofi_5440/data_challenge_2024_sanofi")
 (path<-getwd())
 
 
-##########
+#+++++++++
 #loading##
-##########
+#+++++++++
 
 df_flu<-read_excel(paste0(path,"/Dataset/Consolidated_dataset_MASTER.xlsx"),
                    sheet="Flu",
@@ -27,28 +27,28 @@ df_rsv<-read_excel(paste0(path,"/Dataset/Consolidated_dataset_MASTER.xlsx"),
                    col_types = c("text","text","numeric","numeric","numeric","text","text","numeric","numeric","numeric","numeric","numeric"))
 df_rsv<-df_rsv[df_rsv$Country=="Türkiye",]
 
-covid_raw <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-#https://ourworldindata.org/grapher/daily-covid-cases-deaths?country=~TUR
+# covid_raw <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+# #https://ourworldindata.org/grapher/daily-covid-cases-deaths?country=~TUR
+# 
+# df_covid_raw <- covid_raw %>% 
+#   filter(location=='Turkey') %>%
+#   select(date, new_cases_per_million, total_deaths_per_million) %>%
+#   #hosp_patients, hosp_patients_per_million missing
+#   mutate(year = year(date),
+#          Week_num = isoweek(date)) %>% 
+#   filter(year > 2016 & year < 2024) 
+# 
+# #weekly
+# df_covid <- df_covid_raw %>% 
+#   group_by(year, Week_num) %>%
+#   summarise(new_cases_per_million = sum(new_cases_per_million, na.rm=TRUE),#watch out the NA
+#             .groups = 'drop') %>%
+#   mutate(year_week = paste(year, Week_num, sep = "-"))
 
-df_covid_raw <- covid_raw %>% 
-  filter(location=='Turkey') %>%
-  select(date, new_cases_per_million, total_deaths_per_million) %>%
-  #hosp_patients, hosp_patients_per_million missing
-  mutate(year = year(date),
-         Week_num = isoweek(date)) %>% 
-  filter(year > 2016 & year < 2024) 
 
-#weekly
-df_covid <- df_covid_raw %>% 
-  group_by(year, Week_num) %>%
-  summarise(new_cases_per_million = sum(new_cases_per_million, na.rm=TRUE),#watch out the NA
-            .groups = 'drop') %>%
-  mutate(year_week = paste(year, Week_num, sep = "-"))
-
-
-#####################
+#++++++++++++++++++++
 ## data tangling ####
-#####################
+#++++++++++++++++++++
 
 ##flu ##
 season_week_cut<-20
@@ -74,7 +74,7 @@ df_flu<-df_flu %>% #Added columns with season + season week
   mutate(Year=factor(Year),
          Season=factor(Season),
          Year_week=paste(Year,Week_num,"1",sep='_'),
-         #Date=as.Date(Year_week,'%Y_%W_%u'),#yday 369 in year 2020 is invalid
+         Date=as.Date(Year_week,'%Y_%W_%u'),#yday 369 in year 2020 is invalid
          Year=factor(Year),
          hospitalisation_rate=as.numeric(1000000*hospitalisation_num/Population    ))
 
@@ -97,16 +97,16 @@ df_rsv<-df_rsv %>% #Added columns with season + season week
 
 
 
-##################################
-##plot flu + RSV over 7 years ####
-##################################
+#+++++++++++++++++++++++++++++++++
+##plot 1: flu + RSV over 7 years ####
+#+++++++++++++++++++++++++++++++++
 
 plot_flu_RSV <- ggplot()+
 
-  geom_line(data=df_flu,aes(x=Date,y=hospitalisation_rate),color="skyblue",size=0.7) +
-  geom_line(data=df_rsv,aes(x=Date,y=hospitalisation_rate),color="purple",size=0.7)+
+  geom_line(data=df_flu,aes(x=Date,y=hospitalisation_rate),color="#088199",size=0.7) +
+  geom_line(data=df_rsv,aes(x=Date,y=hospitalisation_rate),color="#f77935",size=0.7)+
   labs(title="Influenza & RSV Hospitalisation Trends in Türkiye",
-       x="Year - week number") +
+       x="Year-week") +
   scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0),
                      limits=c(0,1.2)) +
   scale_x_date(date_labels="%yW%W",date_breaks="2 months",
@@ -119,49 +119,159 @@ plot_flu_RSV <- ggplot()+
 
 print(plot_flu_RSV)
 
-ggsave("Output/graphs/02_Turkiye_flu_rsv_7yeartrend.png",
+ggsave("Output/graphs/04_turkey/01_Turkiye_flu_rsv_7yeartrend.png",
        plot = plot_flu_RSV, 
        width = 11, height = 5, dpi = 300)
 
 
-#######################
-##plot flu overlap ####
-#######################
+#++++++++++++++++++++++++
+##plot 2: flu overlap ####
+#++++++++++++++++++++++++
 
+x_axis_index<-c(seq(21,51,by=2),seq(1,20,by=2))
 
+df_flu_subset<- df_flu[df_flu$Season!="2020/21"&
+                         df_flu$Season!="2023/24", ]
 
-x_axis_index<-df_rsv %>%
-  select(Week_num) %>%
-  slice(1:52) %>%
-  slice(which(row_number() %% 2==1)) %>%
-  mutate(Week_num = as.factor(Week_num))
-
-
-
-tt<-subset(df_rsv,(as.integer(Season) %in% c(2,3,4,7,8)))
-
-
-
-plot_flu<-ggplot(data=  subset(df_flu,
-                               df_flu$Season!="2020/2021"&df_flu$Season!="2021/2022") ,
+plot_flu_overlap<-ggplot(data=  df_flu_subset ,
                 aes(x=Season_week,
                     y=hospitalisation_rate,
                     colour=Season))+
-  geom_line(size=0.8)
-+ 
-  labs(title="Flu hospitalisation rates in England (2017-2023)") + 
-  scale_color_manual(values = c("#f4b18d", "#f77935", "#b1b2b3", 
-                                "#18cdf1", "#088199")) +
+  geom_line(size=0.8)+ 
+  labs(title="Flu hospitalisation rates in Türkiye (2017-2023)") + 
+ scale_color_manual(values = c("#f4b18d", "#f77935", "#b1b2b3",
+                              "#18cdf1", "#088199")) +
   scale_x_continuous(name="Calendar week number",expand=c(0,0),breaks=seq(1,52,by=2),
-                     labels=xaxis_index$Week_num) +
+                     labels=x_axis_index  ) +
   scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0),
-                     limits=c(0,20)) 
-
-+
+                     limits=c(0,1.20)) +
+  
   theme(panel.border = element_blank(), 
         panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
         panel.background = element_rect(fill = "transparent"),
         legend.position=c(0.1,0.7),
         text=element_text(size=14,family="Arial"),
-        plot.title=element_text(hjust=0.5))
+        plot.title=element_text(hjust=0.5),
+        legend.text=element_text(size=15))
+
+print(plot_flu_overlap)
+
+ggsave("Output/graphs/04_turkey/02_turkey_flu_overlap.png",
+       plot = plot_flu_overlap,
+       width = 7, height = 4)
+
+
+#++++++++++++++++++++++++++
+## plot 3: RSV overlap ####
+#++++++++++++++++++++++++++
+
+x_axis_index<-c(seq(21,51,by=2),seq(1,20,by=2))
+
+df_rsv_subset<- df_rsv[df_rsv$Season!="2020/21"&
+                         df_rsv$Season!="2023/24", ]
+
+plot_rsv_overlap<-ggplot(data=  df_rsv_subset ,
+                         aes(x=Season_week,
+                             y=hospitalisation_rate,
+                             colour=Season))+
+  geom_line(size=0.8)+ 
+  labs(title="RSV hospitalisation rates in Türkiye (2017-2023)") + 
+  scale_color_manual(values = c("#f4b18d", "#f77935", "#b1b2b3",
+                                "#18cdf1", "#088199")) +
+  scale_x_continuous(name="Calendar week number",expand=c(0,0),breaks=seq(1,52,by=2),
+                     labels=x_axis_index  ) +
+  scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0),
+                     limits=c(0,0.7)) +
+  
+  theme(panel.border = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        panel.background = element_rect(fill = "transparent"),
+        legend.position=c(0.1,0.7),
+        text=element_text(size=14,family="Arial"),
+        plot.title=element_text(hjust=0.5),
+        legend.text=element_text(size=15))
+
+print(plot_rsv_overlap)
+
+ggsave("Output/graphs/04_turkey/02_turkey_rsv_overlap.png",
+       plot = plot_rsv_overlap,
+       width = 7, height = 4)
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#PLot 4: Strain
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+df_flu$hospitalisation_rate_A<-as.numeric(1000000*df_flu$Flu_A/df_flu$Population    )
+df_flu$hospitalisation_rate_B<-as.numeric(1000000*df_flu$Flu_B/df_flu$Population    )
+
+
+df_flu_subset<- df_flu[df_flu$Season!="2020/21"&
+                         df_flu$Season!="2023/24", ]
+
+
+## A
+plot_flu_overlap_A<-ggplot(data=  df_flu_subset ,
+                         aes(x=Season_week,
+                             y=hospitalisation_rate_A,
+                             colour=Season))+
+  geom_line(size=0.8)+ 
+  labs(title="Influenza A hospitalisation trends") + 
+  scale_color_manual(values = c("#f4b18d", "#f77935", "#b1b2b3",
+                                "#18cdf1", "#088199")) +
+  scale_x_continuous(name="Calendar week number",expand=c(0,0),breaks=seq(1,52,by=2),
+                     labels=x_axis_index  ) +
+  scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0),
+                     limits=c(0,0.9)) +
+  
+  theme(panel.border = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        panel.background = element_rect(fill = "transparent"),
+        legend.position=c(0.1,0.7),
+        text=element_text(size=14,family="Arial"),
+        plot.title=element_text(hjust=0.5),
+        legend.text=element_text(size=15))
+
+## B
+plot_flu_overlap_B<-ggplot(data=  df_flu_subset ,
+                           aes(x=Season_week,
+                               y=hospitalisation_rate_B,
+                               colour=Season))+
+  geom_line(size=0.8)+ 
+  labs(title="Influenza B hospitalisation trends") + 
+  scale_color_manual(values = c("#f4b18d", "#f77935", "#b1b2b3",
+                                "#18cdf1", "#088199")) +
+  scale_x_continuous(name="Calendar week number",expand=c(0,0),breaks=seq(1,52,by=2),
+                     labels=x_axis_index  ) +
+  scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0),
+                     limits=c(0,0.3)) +
+  
+  theme(panel.border = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        panel.background = element_rect(fill = "transparent"),
+        legend.position=c(0.1,0.7),
+        text=element_text(size=14,family="Arial"),
+        plot.title=element_text(hjust=0.5),
+        legend.text=element_text(size=15))
+
+#print
+print(plot_flu_overlap_A)
+print(plot_flu_overlap_B)
+
+#install.packages("ggpubr")
+library(ggpubr)
+
+plot_flu_overlap_AB<-ggarrange(
+         plot_flu_overlap_A, plot_flu_overlap_B, 
+         # labels = c("A", "B"),
+          ncol = 1, nrow = 2)
+
+print(plot_flu_overlap_AB)
+
+
+ggsave("Output/graphs/04_turkey/03_turkey_flu_strain.png",
+       plot = plot_flu_overlap_AB,
+       width = 8, height = 6)
