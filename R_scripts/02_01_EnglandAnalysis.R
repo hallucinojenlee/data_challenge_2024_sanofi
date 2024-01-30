d7002e0bd25869dce9886a4ed9f55dc967ffe79d
gioJacob <giovanni.jacob22@gmail.com>
2024-01-30 01:31:28 +0000

    cleaned up england script + placed all graphs in folders

--- R_scripts/02_01_EnglandAnalysis.R
+++ R_scripts/02_01_EnglandAnalysis.R
@@ -1,9 +1,6 @@
-#####UK DATA EXPLORATION CODE#####
-
-#CODE FOR COUNTRY LEVEL CHART IS AT LINE 158 to 174
-
-getwd()
-setwd("/Users/giojacob/Desktop/HDS_23_24/Data Challenge")
+#####DATA CLEANING AND ANALYSIS FOR ENGLAND#####
+#Updated 30 Jan 2024
+#Author: Gio
 
 #Load packages
 install.packages("ggthemes")
@@ -15,22 +12,28 @@ library(readxl)
 library(scales)
 library(gridExtra)
 
-#Load datasets
-fluraw <- read_excel("~/Library/CloudStorage/OneDrive-LondonSchoolofHygieneandTropicalMedicine/Sanofi/Datasets/Consolidated_dataset_MASTER.xlsx", 
+path<-getwd()
+########################
+#Load data for analysis
+########################
+fluraw <- read_excel(paste0(path,"/Dataset/Consolidated_dataset_MASTER.xlsx"), 
                      sheet = "Flu")
-rsvraw <- read_excel("~/Library/CloudStorage/OneDrive-LondonSchoolofHygieneandTropicalMedicine/Sanofi/Datasets/Consolidated_dataset_MASTER.xlsx", 
+rsvraw <- read_excel(paste0(path,"/Dataset/Consolidated_dataset_MASTER.xlsx"), 
                      sheet = "RSV")
-fluage_18_20 <- read_excel("~/Library/CloudStorage/OneDrive-LondonSchoolofHygieneandTropicalMedicine/Sanofi/Datasets/Consolidated_dataset_MASTER.xlsx", 
+fluage_18_20 <- read_excel(paste0(path,"/Dataset/Consolidated_dataset_MASTER.xlsx"), 
                            sheet = "England_Flu_17_20_age")
-fluage_20 <- read_excel("~/Library/CloudStorage/OneDrive-LondonSchoolofHygieneandTropicalMedicine/Sanofi/Datasets/Consolidated_dataset_MASTER.xlsx", 
+fluage_20 <- read_excel(paste0(path,"/Dataset/Consolidated_dataset_MASTER.xlsx"), 
                         sheet = "UKFlu_20-23_age")
-rsv_age <- read_excel("~/Library/CloudStorage/OneDrive-LondonSchoolofHygieneandTropicalMedicine/Sanofi/Datasets/Consolidated_dataset_MASTER.xlsx", 
+rsv_age <- read_excel(paste0(path,"/Dataset/Consolidated_dataset_MASTER.xlsx"),
                       sheet = "England_RSV_age")
 covid<-read.csv("https://covid.ourworldindata.org/data/owid-covid-data.csv") %>%
   select(location, date,weekly_hosp_admissions,weekly_hosp_admissions_per_million) %>%
   filter(location=="United Kingdom")
 
-#Filter dataset for Flu
+season_week<-read.csv(paste0(path,"/Dataset/seasonweek.csv"))
+########################################################################
+#Data processing - factorise relevant columns, generate date from week-year
+########################################################################
 ukflu_s<-ukflu_s %>% #Added columns with season + season week
   mutate(Year=factor(Year),
          Season=factor(Season),
@@ -47,18 +50,19 @@ ukflu_c<- fluraw %>%
          Year=factor(Year),
          hospitalisation_rate=as.numeric(hospitalisation_rate))
 
-ukflu_c<-left_join(x=ukflu_c,y=season_week,by="Year_week") 
+ukflu_c<-left_join(x=ukflu_c,y=season_week,by="Year_week")
+#left join to generate Flu Season tags + Season week for main charts
 
 ukflu_c<-ukflu_c %>% mutate(Season = factor(ukflu_c$Season),
-                            Year.y = factor(Year.y))
+                            Year.y = factor(Year.y)) #Factorise season + Year
 
-str(ukflu_c)
-
-#Filter out covid years
-ukflu_sub<-ukflu_s %>%
+#Create sub dataset containing only pre and post covid years - unused
+ukflu_sub<-ukflu_c %>%
   filter(as.integer(Season) %in% c(2,3,4,7,8))
 
-#Load rsv data
+############################################
+#Load & pre-process rsv data for analysis
+############################################
 ukrsv_c<- rsvraw %>%
   filter(Country=="England")%>%
   select(Year,Month,Week_num,hospitalisation_rate) %>%
@@ -72,26 +76,30 @@ ukrsv_c<-left_join(x=ukrsv_c,y=season_week,by="Year_week")
 ukrsv_c<-ukrsv_c %>% mutate(Season = factor(ukrsv_c$Season),
          Year.y = factor(Year.y))
 
-str(ukrsv_c)
-
-#Load and process age data post covid
+###################################################################
+#Load & pre-process age breakdown data for analysis (2020 onwards)
+###################################################################
 fluage_20<-fluage_20 %>%
-  mutate(Year_week=paste(Year,`Week number`,"1",sep='_'),
+  mutate(Year_week=paste(Year,`Week number`,"1",sep='_'), #generate date
          Date=as.Date(Year_week,'%Y_%W_%u'),
          Year=factor(Year))
 
-fluage_20<-left_join(x=fluage_20,y=season_week,by="Year_week")
+fluage_20<-left_join(x=fluage_20,y=season_week,by="Year_week") 
+#left join to get season + season week tags
 
+#Pivot data from long to wide to create charts
 fluage_20long<-fluage_20 %>%
   pivot_longer(cols=!c("Year.x","Week number","Year_week","Date",
                        "Season","Season_week","Year.y","Week_num"),
                names_to='age_group',
                values_to='hospitalisation_rate') %>%
-  mutate(age_group=as.factor(age_group)) %>% drop_na()
+  mutate(age_group=as.factor(age_group),
+         Season=factor(Season)) %>% drop_na()
 
-str(fluage_20_long)
-
-#Load and process age data pre covid
+###################################################################
+#Load & pre-process age breakdown data for analysis (2018-2019)
+#Age breakdown is different in earlier years
+###################################################################
 fluage_1820long<-fluage_18_20 %>% 
   pivot_longer(cols=!c("Year","Week"),
                names_to='age_group',
@@ -102,10 +110,12 @@ fluage_1820long<-fluage_18_20 %>%
          age_group=factor(age_group))
 
 fluage_1820long<-left_join(x=fluage_1820long,y=season_week,by="Year_week")
+fluage_1820long<-fluage_1820long %>%
+  mutate(Season=factor(Season))
 
-str(fluage_1820long)
-
-#Data exploration/summary
+##############################################################
+#Summary tables - peak hosp week by flu & RSV season
+##############################################################
 ukflu_sum<-ukflu_c %>%
   group_by(Season) %>% 
   summarise(Maximum=max(hospitalisation_rate),
@@ -126,19 +136,15 @@ age_1820sum<-fluage_1820long %>%
   summarise(Maximum=max(hospitalisation_cases),
             Max_week=Year_week[which.max(hospitalisation_cases)])
 
-write.csv(age_sum,
-          "/Users/giojacob/Desktop/HDS_23_24/Data Challenge/englandagesummary20.png")
-
-write.csv(age_1820sum,
-          "/Users/giojacob/Desktop/HDS_23_24/Data Challenge/englandagesummary1820.png")
-
+########################################################################
 #PLOT MAIN FLU GRAPH - England Flu Season By Year comparison
-#Generate column to label x axis with Calendar week
+########################################################################
+#Generate column to label x axis with Calendar week instead of season week
 xaxis_index<-ukflu_sub %>%
-  select(Week_num) %>%
+  select(Week_num.x) %>%
   slice(1:52) %>%
-  slice(which(row_number() %% 2==1)) %>%
-  mutate(Week_num = as.factor(Week_num))
+  slice(which(row_number() %% 2==1)) %>% #To align with axis markings which only shows every 2 weeks
+  mutate(Week_num.x = as.factor(Week_num.x))
 
 ###MAIN FLU SEASON TREND 
 flu_sub<-ggplot(data=subset(ukflu_c,(as.integer(Season) %in% c(2,3,4,7,8))),
@@ -148,7 +154,7 @@ flu_sub<-ggplot(data=subset(ukflu_c,(as.integer(Season) %in% c(2,3,4,7,8))),
   scale_color_manual(values = c("#f4b18d", "#f77935", "#b1b2b3", 
                                 "#18cdf1", "#088199")) +
   scale_x_continuous(name="Calendar week number",expand=c(0,0),breaks=seq(1,52,by=2),
-                     labels=xaxis_index$Week_num) +
+                     labels=xaxis_index$Week_num.x) +
   scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0),
                      limits=c(0,20)) +
   theme(panel.border = element_blank(), 
@@ -160,11 +166,12 @@ flu_sub<-ggplot(data=subset(ukflu_c,(as.integer(Season) %in% c(2,3,4,7,8))),
         plot.title=element_text(hjust=0.5))
 
 flu_sub #only showing pre post covid trends
-ggsave("/Users/giojacob/Desktop/HDS_23_24/Data Challenge/england_flu.png",
+ggsave(paste0(path,"/Output/graphs/02_England/england_flu.png"),
        plot=flu_sub,width=7,height=4)
 
-#RSV plots
-###MAIN RSV PLOT - by season
+########################################################################
+#PLOT MAIN RSV GRAPH - England RSV Season By Year comparison
+########################################################################
 rsv_sub<-ggplot(data=subset(ukrsv_c,as.integer(Season) %in% c(1,2,3,6,7)),
                 aes(x=Season_week,y=hospitalisation_rate,
                               colour=Season))+
@@ -173,7 +180,7 @@ rsv_sub<-ggplot(data=subset(ukrsv_c,as.integer(Season) %in% c(1,2,3,6,7)),
   scale_color_manual(values = c("#f4b18d", "#f77935", "#b1b2b3", 
                                 "#18cdf1", "#088199")) +
   scale_x_continuous(name="Calendar week number",expand=c(0,0),breaks=seq(1,52,by=2),
-                     labels=xaxis_index$Week_num) +
+                     labels=xaxis_index$Week_num.x) +
   scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0)) +
   theme(panel.border = element_blank(), 
         panel.grid.minor = element_blank(),
@@ -184,15 +191,13 @@ rsv_sub<-ggplot(data=subset(ukrsv_c,as.integer(Season) %in% c(1,2,3,6,7)),
         plot.title=element_text(hjust=0.5))
 
 rsv_sub #only showing pre post covid trends
-ggsave("/Users/giojacob/Desktop/HDS_23_24/Data Challenge/england_rsv.png",
+ggsave(paste0(path,"/Output/graphs/02_England//england_rsv.png"),
        plot=rsv_sub,width=7,height=4)
 
 #COVID DATA EXPLORATION
-covid_c <- na.omit(covid) %>%
-  mutate(date=as.Date(date))
-str(covid_c)
+covid_c <- na.omit(covid) %>% mutate(date=as.Date(date))
 
-#Combined plot - flu, rsv, covid
+#Combined plot - flu, rsv, covid - unused
 combplot<-ggplot() +
   geom_line(data=ukflu_s,aes(x=Date,y=hospitalisation_rate),color="skyblue",size=0.7) +
   geom_line(data=ukrsv_c,aes(x=Date,y=hospitalisation_rate),color="purple",size=0.7) +
@@ -211,7 +216,9 @@ combplot<-ggplot() +
         axis.text.x = element_text(angle=90, vjust = 0.5, hjust=1),
         plot.title=element_text(hjust=0.5))
 
-#Combined plot - flu and rsv only
+########################################################
+#Combined time series plot - Flu & RSV only
+########################################################
 flu_rsv<-ggplot() +
   geom_line(data=ukflu_c,aes(x=Date,y=hospitalisation_rate),color="#088199",size=0.7) +
   geom_line(data=ukrsv_c,aes(x=Date,y=hospitalisation_rate),color="#f77935",size=0.7) +
@@ -227,24 +234,27 @@ flu_rsv<-ggplot() +
         plot.title=element_text(hjust=0.5))
 
 flu_rsv
-ggsave("/Users/giojacob/Desktop/HDS_23_24/Data Challenge/flursvplot.png",plot=flu_rsv,width=11,height=4)
+ggsave(paste0(path,"/Output/graphs/02_England/flursvplot.png"),plot=flu_rsv,width=11,height=4)
 
-#Flu variant exploration
+########################################################
+#Flu variant data pre-processing and cleaning
+########################################################
 fluvar<-ukflu %>%
+  #Filter for only relevant columns
   select(Year,Month,Week_num,hospitalisation_rate,Flu_A,Flu_B) %>%
+  #Generate date based on year and weeks
   mutate(Year_week=paste(Year,Week_num,"1",sep='_'),
          Date=as.Date(Year_week,'%Y_%W_%u'),
+         #Factor year and change rate to numeric
          Year=factor(Year),
          hospitalisation_rate=as.numeric(hospitalisation_rate))
 
 fluvar<-left_join(x=fluvar,y=season_week,by="Year_week")
 fluvar<-fluvar %>% mutate(Season=factor(Season))
 
-fluvar$prop_fluA <- fluvar$Flu_A/(fluvar$Flu_A+fluvar$Flu_B)
-
-str(fluvar)
-
-#Flu variant plots
+########################################################
+#FLU VARIANT TRENDS BY SEASON
+########################################################
 flu_b<-ggplot(data=subset(fluvar,as.integer(Season) %in% c(3,4,7,8)),
               aes(x=Season_week,color=Season)) +
   geom_line(aes(y=Flu_B),size=0.7) +
@@ -282,49 +292,74 @@ flu_a<-ggplot(data=subset(fluvar,as.integer(Season) %in% c(3,4,7,8)),
 flu_b
 
 var_plot<-grid.arrange(flu_a,flu_b,nrow=2)
-ggsave("/Users/giojacob/Desktop/HDS_23_24/Data Challenge/Variants.png",
+ggsave(paste0(path,"/Output/graphs/02_England/Variants.png"),
        var_plot,height=6,width=8,bg='transparent')
 
-#Plot age differences
-ggplot(data=fluage20_senior,aes(x=Season_week,y=hospitalisation_rate,
-                                colour=Season)) +
-  geom_line(size=0.7) +
-  labs(title="Influenza hospitalisation trends in UK by age") +
-  scale_colour_brewer(palette="Dark2") +
-  scale_y_continuous(name="Hospitalisation rate (per 100k)",expand=c(0,0)) +
-  scale_x_continuous(expand=c(0,0)) +
-  theme(panel.grid.major = element_blank(),
-        axis.line = element_line(colour = "black"),
-        panel.background = element_rect(fill = "white"),
-        axis.text.x = element_text(angle=90, vjust = 0.5, hjust=1),
-        plot.title=element_text(hjust=0.5))
+########################################################
+#FLU HOSPITALISATION TRENDS BY AGE
+########################################################
+#Plot for pre-covid baseline
+agepre<-ggplot(subset(fluage_1820long,as.integer(Season) %in% c(1,2,3)), #select only 2022-23
+               aes(x=Week_num, y=hospitalisation_cases)) +
+  #group by flu season
+  geom_line(aes(color=Season), size=.8) +
+  #put individual graphs by age group into one visualisation
+  facet_wrap(~ age_group,scales='free_y') +
+  #set colors for flu season
+  scale_color_manual(values = c("#f4b18d", "#f77935","#b1b2b3")) + 
+  #set labels
+  xlab("Week (since begining of season)") + 
+  ylab("Rate of Hospitalisations (per 100k)") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(color = "Season") +  
+  #set plot design
+  theme_minimal() +  
+  scale_y_continuous(expand=c(0,0)) + 
+  theme(panel.border = element_blank(),  
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),  
+        text=element_text(size=14, 
+                          family="Arial"), 
+        axis.line = element_line(colour = "black")) 
 
-#Do facet plot
-ageplot_post<-ggplot(data=subset(fluage_20_long,as.integer(Season) %in% c(6,7,8)),
-                     aes(x=Season_week,
-                         y=hospitalisation_rate,
-                         color=Season)) + 
+ggsave(paste0(path,"/Output/graphs/02_England/age_pre.png"),agepre,width=6,height=6)
+
+#######
+#create plot (2022-23) - y-axis = hospitalisations, x-axis = Season week
+#######
+agepost<-ggplot(subset(fluage_20long,as.integer(Season) %in% c(2,3)), #select only 2022-23
+       aes(x=Week_num, y=hospitalisation_rate)) +
+  #group by flu season
+  geom_line(aes(color=Season), size=.8) +
+  #put individual graphs by age group into one visualisation
+  facet_wrap(~ age_group) +
+  #set colors for flu season
+  scale_color_manual(values = c("#18cdf1", "#088199")) + 
+  #set labels
+  xlab("Week (since begining of season)") + 
+  ylab("Rate of Hospitalisations (per 100k)") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(color = "Season") +  
+  #set plot design
+  theme_minimal() +  
+  scale_y_continuous(expand=c(0,0)) + 
+  theme(panel.border = element_blank(),  
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),  
+        text=element_text(size=14, 
+                          family="Arial"), 
+        axis.line = element_line(colour = "black")) 
+
+ggsave(paste0(path,"/Output/graphs/02_England/age_post.png"),agepost,width=6,height=6)
+################################################################
+#Alternative viz for for hosp, age breakdown and flu season
+# Plot is facet wrapped by age and flu season
+################################################################
+ggplot(data=subset(fluage_20_long,as.integer(Season) %in% c(6,7,8)),
+                     aes(x=Season_week,y=hospitalisation_rate, color=Season)) + 
   geom_line() + 
-  facet_grid(rows=vars(age_group),
-             col=vars(Season),
-             scales='free_y') +
+  facet_grid(rows=vars(age_group),col=vars(Season),scales='free_y') +
   theme(axis.line = element_line(colour = "black"),
         panel.background = element_rect(fill = "white"),
         axis.text.x = element_text(angle=90, vjust = 0.5, hjust=1),
         plot.title=element_text(hjust=0.5))
-
-ageplot_pre<-ggplot(data=subset(fluage_1820long,as.integer(Season) %in% c(2,3)),
-                    aes(x=Season_week,
-                        y=hospitalisation_cases,
-                        color=Season)) +
-  geom_line() + 
-  facet_grid(rows=vars(age_group),
-             cols=vars(Season),
-             scales='free_y') +
-  theme(axis.line = element_line(colour = "black"),
-        panel.background = element_rect(fill = "white"),
-        axis.text.x = element_text(angle=90, vjust = 0.5, hjust=1),
-        plot.title=element_text(hjust=0.5))
-
-ggsave("age_post.png",ageplot_post,width=6,height=6)
-ggsave("age_pre.png",ageplot_pre,width=6,height=6)
