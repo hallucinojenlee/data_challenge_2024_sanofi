e0d14a8245a949d939868e777b6f18fc2bd21edf
Dhihram <105160118+Dhihram@users.noreply.github.com>
2024-02-01 20:30:39 +0000

    Update 07_03_brazil_variance_and_comparisons_plot.R

--- R_scripts/07_03_brazil_variance_and_comparisons_plot.R
+++ R_scripts/07_03_brazil_variance_and_comparisons_plot.R
@@ -51,13 +51,13 @@ x_p <- x_p[!x_p$Week_num == "53", ]
 everyother <- function(x) x[seq_along(x) %% 4 == 0]
 ggplot() +
   geom_line(data = x_p , aes(x = Week_num, `2019/2020`, group = 1, color = '2019/2020'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2021/2022`, group = 1, color = '2022/2023'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2023/2024'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2022/2023'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2023/2024`, group = 1, color = '2023/2024'), size = 1) +
   scale_x_discrete(name = 'Calendar week number', breaks = everyother) + scale_color_manual('Year',values = c("#b1b2b3",  
                                                                                                               "#18cdf1", "#088199")) +
   
   scale_y_continuous(name="No of Hospitalisation",expand=c(0,0),
-                     limits=c(0,3000)) +
+                     limits=c(0,1200)) +
   theme(panel.border = element_blank(), 
         panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
@@ -110,13 +110,13 @@ x_p <- x_p[!x_p$Week_num == "53", ]
 everyother <- function(x) x[seq_along(x) %% 4 == 0]
 ggplot() +
   geom_line(data = x_p , aes(x = Week_num, `2019/2020`, group = 1, color = '2019/2020'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2021/2022`, group = 1, color = '2022/2023'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2023/2024'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2022/2023'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2023/2024`, group = 1, color = '2023/2024'), size = 1) +
   scale_x_discrete(name = 'Calendar week number', breaks = everyother) + scale_color_manual('Year', values = c("#b1b2b3",  
                                                                                                                "#18cdf1", "#088199")) +
   
   scale_y_continuous(name="No of Hospitalisation",expand=c(0,0),
-                     limits=c(0,1000)) +
+                     limits=c(0,1500)) +
   theme(panel.border = element_blank(), 
         panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
@@ -155,8 +155,43 @@ ggplot() +
 
 
 #flu A and B
-
+mast <- import_list(path) 
+flu <- mast$Flu
+flu <- flu %>% filter(Country == 'Brazil')
+flu$Week_date <- as.numeric(flu$Week_date)
+flu$Week_date <- as.Date(flu$Week_date, origin = "1899-12-30")
+flu$Week_num <- isoweek(flu$Week_date)
+flu$Year <- isoyear(flu$Week_date)
+brazil_clean <- flu
 brazil_clean <- as.data.frame(brazil_clean)  
+#reading
+mast <- import_list(path) 
+flu <- mast$Flu
+flu <- flu %>% filter(Country == 'Brazil')
+flu$Week_date <- as.numeric(flu$Week_date)
+flu$Week_date <- as.Date(flu$Week_date, origin = "1899-12-30")
+flu$Week_num <- isoweek(flu$Week_date)
+flu$Year <- isoyear(flu$Week_date)
+brazil_clean <- flu
+brazil_clean[brazil_clean$Month == 12 & brazil_clean$Week_num == 1,"Month"] <- 1
+brazil_clean <- brazil_clean %>% group_by(Year, Month, Week_num, Week_date, hospitalisation_rate) %>% 
+  summarise(hospitalisation_num = sum(hospitalisation_num, na.rm = TRUE))
+
+
+#comparison
+brazil_clean$kat_year <- NA
+brazil_clean[brazil_clean$Week_num %in% c(9:53) & brazil_clean$Year %in% c(2019),"kat_year"] <- '2019/2020'
+brazil_clean[brazil_clean$Week_num %in% c(1:8) & brazil_clean$Year %in% c(2020),"kat_year"] <- '2019/2020'
+brazil_clean[brazil_clean$Week_num %in% c(9:53) & brazil_clean$Year %in% c(2020),"kat_year"] <- '2020/2021'
+brazil_clean[brazil_clean$Week_num %in% c(1:8) & brazil_clean$Year %in% c(2021),"kat_year"] <- '2020/2021'
+brazil_clean[brazil_clean$Week_num %in% c(9:53) & brazil_clean$Year %in% c(2021),"kat_year"] <- '2021/2022'
+brazil_clean[brazil_clean$Week_num %in% c(1:8) & brazil_clean$Year %in% c(2022),"kat_year"] <- '2021/2022'
+brazil_clean[brazil_clean$Week_num %in% c(9:53) & brazil_clean$Year %in% c(2022),"kat_year"] <- '2022/2023'
+brazil_clean[brazil_clean$Week_num %in% c(1:8) & brazil_clean$Year %in% c(2023),"kat_year"] <- '2022/2023'
+brazil_clean[brazil_clean$Week_num %in% c(9:53) & brazil_clean$Year %in% c(2023),"kat_year"] <- '2023/2024'
+brazil_clean[brazil_clean$Week_num %in% c(1:8) & brazil_clean$Year %in% c(2024),"kat_year"] <- '2023/2024'
+brazil_clean <- brazil_clean[-c(1:8), ]
+
 ggplot(data = brazil_clean, aes(x=Week_num, y=Flu_B)) + geom_line(aes(colour=kat_year)) + theme_minimal()
 x <- brazil_clean %>% select(Week_num, Flu_A, kat_year) 
 
@@ -170,13 +205,13 @@ x_p <- x_p[!x_p$Week_num == "53", ]
 everyother <- function(x) x[seq_along(x) %% 4 == 0]
 a <- ggplot() +
   geom_line(data = x_p , aes(x = Week_num, `2019/2020`, group = 1, color = '2019/2020'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2021/2022`, group = 1, color = '2022/2023'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2023/2024'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2022/2023'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2023/2024`, group = 1, color = '2023/2024'), size = 1) +
   scale_x_discrete(name = 'Season week number', breaks = everyother) + scale_color_manual('Year',values = c("#b1b2b3",  
                                                                                                             "#18cdf1", "#088199")) +
   
   scale_y_continuous(name="Hospitalisation",expand=c(0,0),
-                     limits=c(0,1500)) +
+                     limits=c(0,300)) +
   theme(panel.border = element_blank(), 
         panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
@@ -196,8 +231,8 @@ x_p <- x_p[!x_p$Week_num == "53", ]
 everyother <- function(x) x[seq_along(x) %% 4 == 0]
 b <- ggplot() +
   geom_line(data = x_p , aes(x = Week_num, `2019/2020`, group = 1, color = '2019/2020'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2021/2022`, group = 1, color = '2022/2023'), size = 1) +
-  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2023/2024'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2022/2023`, group = 1, color = '2022/2023'), size = 1) +
+  geom_line(data = x_p , aes(x = Week_num, `2023/2024`, group = 1, color = '2023/2024'), size = 1) +
   scale_x_discrete(name = 'Season week number', breaks = everyother) + scale_color_manual('Year',values = c("#b1b2b3",  
                                                                                                             "#18cdf1", "#088199")) +
   
@@ -210,4 +245,3 @@ b <- ggplot() +
         legend.position=c(0.1,0.9),
         text=element_text(size=14,family="Arial"),
         plot.title=element_text(hjust=0.5)) 
-
